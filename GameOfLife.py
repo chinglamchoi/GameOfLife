f815d97c0f9499e862181f11c3a177542e66ee83
Ching Lam Choi <ccl5a09@gmail.com>
2017-07-30 11:06:57 -0700

    Patch 1
    While loops instead of for, still has syntax error on line 27 "index out of range"

--- GameOfLife.py
+++ GameOfLife.py
@@ -10,52 +10,57 @@ the_list = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 dc = 0 #dc == alive neighbouring cells next to dead cell
 ac = 0 #ac == alive neighbouring cells next to alive cell
-m = 0  #m is thelist[X][?]
-n = 0  #n is thelist[?][X]
+m = 1  #m is thelist[X][?]
+n = 1  #n is thelist[?][X]
+g = m + 1
+h = n + 1
+j = m - 1
+k = n -1
+cell = the_list[n][m]
 # unecessary: noofn = len(the_list) #checks no. of arrays in list
 # unecessary: noofm = len(the_list[0]) # checks the no. of values in each array
 
 
-for i in range(12): # wrong or not...
-    for j in range(12): #need to add exception for when cell doesn't have neighbours +
-        if the_list[n][m] == 0: # if cell == dead, check neighbours
-            if the_list[n][m+1] == 1: #when code is run: outputs "IndexError: list index out of range"
+while n > 0 and n < 11: # wrong or not...
+    while m > 0 and m < 11: #need to add exception for when cell doesn't have neighbours +
+        if cell == 0: # if cell == dead, check neighbours
+            if the_list[n][g] == 1: #when code is run: outputs "IndexError: list index out of range"
                 dc = dc + 1 
-            if the_list[n][m-1] == 1:
+            if the_list[n][j] == 1:
                 dc = dc + 1
-            if the_list[n+1][m] == 1:
+            if the_list[h][m] == 1:
                 dc = dc + 1
-            if the_list[n+1][m+1] == 1:
+            if the_list[h][g] == 1:
                 dc = dc + 1
-            if the_list[n+1][m-1] == 1:
+            if the_list[h][j] == 1:
                 dc = dc + 1
-            if the_list[n-1][m] == 1:
+            if the_list[k][m] == 1:
                 dc = dc + 1
-            if the_list[n-1][m+1] == 1:
+            if the_list[k][g] == 1:
                 dc = dc + 1
-            if the_list[n-1][m-1] == 1:
+            if the_list[k][j] == 1:
                 dc = dc + 1
                 if dc == 3: # if == 3 neighbours, revive
-                    the_list[n][m] = 1
-        elif the_list[n][m] == 1: # if cell == alive, check neighbours
-            if the_list[n][m+1] == 1:
+                    cell = 1
+        elif cell == 1: # if cell == alive, check neighbours
+            if the_list[n][g] == 1:
                 ac = ac + 1 
-            if the_list[n][m-1] == 1:
+            if the_list[n][j] == 1:
                 ac = ac + 1
-            if the_list[n+1][m] == 1:
+            if the_list[h][m] == 1:
                 ac = ac + 1
-            if the_list[n+1][m+1] == 1:
+            if the_list[h][g] == 1:
                 ac = ac + 1
-            if the_list[n+1][m-1] == 1:
+            if the_list[h][j] == 1:
                 ac = ac + 1
-            if the_list[n-1][m] == 1:
+            if the_list[k][m] == 1:
                 ac = ac + 1
-            if the_list[n-1][m+1] == 1:
+            if the_list[k][g] == 1:
                 ac = ac + 1
-            if the_list[n-1][m-1] == 1:
+            if the_list[k][j] == 1:
                 ac = ac + 1
                 if ac != 2 and ac != 3: # if alive neighbours != both 2 and 3, cell is dead
-                    the_list[n][m] = 0
+                    cell = 0
             m = m + 1 #checks next value in the_list[n]
         n = n + 1
 
