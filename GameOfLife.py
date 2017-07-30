72a9912ceead4605132681b96d2bd31fa41d9943
Ching Lam Choi <ccl5a09@gmail.com>
2017-07-30 15:37:58 -0700

    Update GameOfLife.py

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,13 +1,14 @@
-the_list = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+the_list = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 dc = 0 #dc == alive neighbouring cells next to dead cell
 ac = 0 #ac == alive neighbouring cells next to alive cell
 m = 1  #m is thelist[X][?]
@@ -17,12 +18,9 @@ h = n + 1
 j = m - 1
 k = n -1
 cell = the_list[n][m]
-# unecessary: noofn = len(the_list) #checks no. of arrays in list
-# unecessary: noofm = len(the_list[0]) # checks the no. of values in each array
 
-
-while n > 0 and n < 11: # wrong or not...
-    while m > 0 and m < 11: #need to add exception for when cell doesn't have neighbours +
+while n > 0 and n < 10: # wrong or not...
+    while m > 0 and m < 10: #need to add exception for when cell doesn't have neighbours +
         if cell == 0: # if cell == dead, check neighbours
             if the_list[n][g] == 1: #when code is run: outputs "IndexError: list index out of range"
                 dc = dc + 1 
@@ -40,8 +38,8 @@ while n > 0 and n < 11: # wrong or not...
                 dc = dc + 1
             if the_list[k][j] == 1:
                 dc = dc + 1
-                if dc == 3: # if == 3 neighbours, revive
-                    cell = 1
+            if dc == 3: # if == 3 neighbours, revive
+                cell = 1
         elif cell == 1: # if cell == alive, check neighbours
             if the_list[n][g] == 1:
                 ac = ac + 1 
@@ -59,10 +57,10 @@ while n > 0 and n < 11: # wrong or not...
                 ac = ac + 1
             if the_list[k][j] == 1:
                 ac = ac + 1
-                if ac != 2 and ac != 3: # if alive neighbours != both 2 and 3, cell is dead
-                    cell = 0
-            m = m + 1 #checks next value in the_list[n]
-        n = n + 1
+            if ac != 2 and ac != 3: # if alive neighbours != both 2 and 3, cell is dead
+                cell = 0
+        m = m + 1 #checks next value in the_list[n]
+    n = n + 1
 
 
 
