7ab74fa38698b2a96c1d10f33cc14df9261780ae
Ching Lam Choi <ccl5a09@gmail.com>
2017-08-24 13:52:00 +0800

    Update GameOfLife.py

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,75 +1,102 @@
 the_list = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-dc = 0 #dc == alive neighbouring cells next to dead cell
-ac = 0 #ac == alive neighbouring cells next to alive cell
-m = 0  #m is thelist[X][?]
-n = 0  #n is thelist[?][X]
 
-for i in range(12): # wrong or not...
-    for j in range(12): #need to add exception for when cell doesn't have neighbours +
-        if the_list[n][m] == 0: # if cell == dead, check neighbours
-            if the_list[n][m+1] == 1: #when code is run: outputs "IndexError: list index out of range"
-                dc = dc + 1 
-            if the_list[n][m-1] == 1:
-                dc = dc + 1
-            if the_list[n+1][m] == 1:
-                dc = dc + 1
-            if the_list[n+1][m+1] == 1:
-                dc = dc + 1
-            if the_list[n+1][m-1] == 1:
-                dc = dc + 1
-            if the_list[n-1][m] == 1:
-                dc = dc + 1
-            if the_list[n-1][m+1] == 1:
-                dc = dc + 1
-            if the_list[n-1][m-1] == 1:
-                dc = dc + 1
-                if dc == 3: # if == 3 neighbours, revive
-                    the_list[n][m] = 1
-        elif the_list[n][m] == 1: # if cell == alive, check neighbours
-            if the_list[n][m+1] == 1:
-                ac = ac + 1 
-            if the_list[n][m-1] == 1:
-                ac = ac + 1
-            if the_list[n+1][m] == 1:
-                ac = ac + 1
-            if the_list[n+1][m+1] == 1:
-                ac = ac + 1
-            if the_list[n+1][m-1] == 1:
-                ac = ac + 1
-            if the_list[n-1][m] == 1:
-                ac = ac + 1
-            if the_list[n-1][m+1] == 1:
-                ac = ac + 1
-            if the_list[n-1][m-1] == 1:
-                ac = ac + 1
-                if ac != 2 and ac != 3: # if alive neighbours != both 2 and 3, cell is dead
-                    the_list[n][m] = 0
-            m = m + 1 #checks next value in the_list[n]
-        n = n + 1
+the_other_list = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+dc = 0
+ac = 0
+m = 1
+n = 1
+count = 0
+count2 = 0
+count3 = 0
+for i in the_list:
+    print(the_list[count2])
+    count2 = count2 + 1
+print("")
+    
+while True:
+    count = 0
+    count3 = 0
+    for n in range(1,10):
+        k = n - 1
+        h = n + 1
+        for m in range(1,10):
+            dc = 0
+            ac = 0
+            g = m + 1
+            j = m - 1
+            cell = the_list[n][m]
+            if the_list[n][m] == 0:
+                if the_list[n][g] == 1:
+                    dc = dc + 1
+                if the_list[n][j] == 1:
+                    dc = dc + 1
+                if the_list[h][m] == 1:
+                    dc = dc + 1
+                if the_list[h][g] == 1:
+                    dc = dc + 1
+                if the_list[h][j] == 1:
+                    dc = dc + 1
+                if the_list[k][m] == 1:
+                    dc = dc + 1
+                if the_list[k][g] == 1:
+                    dc = dc + 1
+                if the_list[k][j] == 1:
+                    dc = dc + 1
+                if dc == 3:
+                    the_other_list[n][m] = 1
+                else:
+                    the_other_list[n][m] = 0
+            if the_list[n][m] == 1:
+                if the_list[n][g] == 1:
+                    ac = ac + 1
+                if the_list[n][j] == 1:
+                    ac = ac + 1
+                if the_list[h][m] == 1:
+                    ac = ac + 1
+                if the_list[h][g] == 1:
+                    ac = ac + 1
+                if the_list[h][j] == 1:
+                   ac = ac + 1
+                if the_list[k][m] == 1:
+                    ac = ac + 1
+                if the_list[k][g] == 1:
+                    ac = ac + 1
+                if the_list[k][j] == 1:
+                    ac = ac + 1
+                if ac != 2 and ac != 3:
+                    the_other_list[n][m] = 0
+                else:
+                    the_other_list[n][m] = 1
 
-
-
-
-
-
-
-#might need to use this as function... def niceprint(the_list):
-#count = 0
-#for i in the_list:
-#    print(the_list[count])
-#    count = count + 1
-
-
-
-        
-            
+    for i in the_other_list:
+        print(the_other_list[count])
+        count = count + 1
+    print("")
+    for x in range(len(the_other_list)):
+        for y in range(len(the_other_list)):
+            the_list[x][y] = the_other_list[x][y]
+    #the_list = the_other_list
+    #for i in the_list:
+     #   print(the_list[count3])
+      #  count3 = count3 + 1
+       # print("")
